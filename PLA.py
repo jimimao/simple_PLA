8d125ae6d642724726d4658c4166af6aa02a24ee
jimimao <45704079+jimimao@users.noreply.github.com>
2019-09-01 15:15:07 +0800

    Add files via upload

--- PLA.py
+++ PLA.py
@@ -14,21 +14,30 @@ def loadmat(name):
 
 #权重矩阵初始化
 def defineweight(n):
-    w = [0] * (n)
+    w = [0] * (n+1)
     w = np.mat(w)
     return w
 
 # t 为阈值
-def predict(weight,X,t):
+def predict(weight,data):
+    a = np.array([1] * (data.shape[0]))
+    X = np.array(data)
+    X = np.insert(X,0,values=a,axis=1)
+    # print(X)
+    #给数据添加上一行1，乘上权重的第一个数值，表示最后的  -threshold，
     res = np.mat(weight) * np.transpose(np.mat(X)) ## * == np.dot(),矩阵乘法
-    prediction = np.sign(res + t)
+    prediction = np.sign(res)
     return prediction
 
 #更新权重矩阵
 def renewweight(weight,prediction,target,lr,Train_data,m):
+    a = np.array([1] * Train_data.shape[0])
+    X = np.array(Train_data)
+    X = np.insert(X,0,values=a,axis=1)
+
     prediction = np.transpose(prediction)
     for i in range(0,m):
-        weight = np.mat(weight) + lr * (target[i] - prediction[i]) * np.mat(Train_data[i])
+        weight = np.mat(weight) + lr * (target[i] - prediction[i]) * np.mat(X[i])
     return weight
 
 
@@ -42,9 +51,9 @@ def acc(pre,data_target):
 #开始训练
 if __name__ == "__main__":
     #超参数
-    lr = 0.01
+    lr = 0.001
     epoch = 100
-    t = 0.1  # threshold
+    # t = -0.1  # threshold
 
     #正确率
     acclist = []
@@ -59,22 +68,22 @@ if __name__ == "__main__":
     prediction = [0]*m
     for i in range(0,epoch):
         # 预测结果
-        prediction = predict(weight,Train_data,t)
+        prediction = predict(weight,Train_data)
         # 对于一条数据的 n 个维度，都要更新权重, t作为threshold 也要更新
         weight = renewweight(weight, prediction, Train_target, lr, Train_data,m)
         # print(weight)
         #利用目前的weight与threshold，对现有的训练数据集进行预测，并记录
-        pre_train = predict(weight,Train_data,t)
+        pre_train = predict(weight,Train_data)
         acc_train = acc(pre_train,Train_target)
         # print(pre_train)
         print("TrainACC%d:%.5f"%(i,acc_train))
         acclist.append(acc_train * 100)
-
+        print(weight[0,0])
     ##测试
     print("Test ACC")
     mt = Test_data.shape[0] #数据 训练集长度
     nt = Test_data.shape[1] #数据 维度
-    pre = predict(weight,Test_data,t) # pre 为1 * mt，Test_target 为mt *1
+    pre = predict(weight,Test_data) # pre 为1 * mt，Test_target 为mt *1
     acc_test = acc(pre,Test_target)
     print("%.5f"%acc_test)
 
